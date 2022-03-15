946176f6cc3691423c78744c5f600cd718593a67
jiyo mathew <jmathe15@its.jnj.com>
2022-03-15 12:10:46 +0530

    user"

--- server/conroller/UserController.js
+++ server/conroller/UserController.js
@@ -1,7 +1,7 @@
 const mongoose = require("mongoose");
 const userModel = require("../model");
 
-mongoose.connect("mongodb://localhost:27017/usersdb", {
+mongoose.connect("mongodb://mongodb:27017/usersdb", {
   useNewUrlParser: true,
   useFindAndModify: false,
   useUnifiedTopology: true,
